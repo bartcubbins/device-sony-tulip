1b54443e3f6ea2bd5c7a7bb38381428d59bc69e6
Pavel Dubrova <pashadubrova@gmail.com>
2018-09-18 11:37:30 +0000

    reorder properties for DS devices
    The ro.telephony.default_network was not properly set without the
    reorder
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- aosp_e2333.mk
+++ aosp_e2333.mk
@@ -12,9 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, device/sony/tulip/aosp_e2303.mk)
-
 # Reserve space for data encryption (12213813248-16384)
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 12213796864
 
@@ -23,6 +20,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.multisim.config=dsds \
     persist.radio.multisim.config=dsds \
     ro.telephony.default_network=9,1
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/sony/tulip/aosp_e2303.mk)
 
 PRODUCT_NAME := aosp_e2333
 PRODUCT_DEVICE := tulip
